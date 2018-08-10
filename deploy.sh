f5cd43a9392599337f2fded4b1289b2ce52b2721
Rex Lee(李俊) <duguying2008@gmail.com>
2018-08-10 12:35:32 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,6 @@ cd ..
 pwd
 gitbook build parsing-techniques parsing-techniques-static
 tar zcvf parsing-techniques.tar.gz parsing-techniques-static/*
+du -sh parsing-techniques.tar.gz
 curl -X POST http://api.duguying.net/api/deploy/upload -H 'Cache-Control: no-cache' -H 'Token: '$token -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' -H 'name: book' -F file=@parsing-techniques.tar.gz
 cd parsing-techniques
