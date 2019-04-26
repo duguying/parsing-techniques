15ff3fe6fec973555b88c65a9c777a519ea34abe
Rex Lee(李俊) <root@duguying.net>
2019-04-26 11:06:30 +0800

    update ci script and config

---
+++ ci.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+WORKSPACE=$(cd $(dirname $0)/; pwd)
+cd $WORKSPACE
+
+function help() {
+    echo "$0 compile|deploy"
+}
+
+function compile() {
+    gitbook install
+    cd ..
+    pwd
+    gitbook build parsing-techniques parsing-techniques-static
+    cd parsing-techniques-static
+    tar zcvf parsing-techniques.tar.gz ./*
+    du -sh parsing-techniques.tar.gz
+}
+
+function deploy() {
+    curl -X POST http://api.duguying.net/api/deploy/upload -H 'Cache-Control: no-cache' -H 'Token: '$token -H 'name: book' -F file=@parsing-techniques.tar.gz
+    cd ../parsing-techniques
+}
+
+if [ "$1" == "" ]; then
+    help
+elif [ "$1" == "complie" ];then
+    compile
+elif [ "$1" == "deploy" ];then
+    deploy
+else
+    help
+fi
